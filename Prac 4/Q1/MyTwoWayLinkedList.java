94003c5f5c029c624da109f1a49144e7cecc287a
Irfan <50040808+Fanos-dev@users.noreply.github.com>
2024-09-09 21:47:03 +0200

    Prac 4

---
+++ Prac 4/Q1/MyTwoWayLinkedList.java
@@ -0,0 +1,307 @@
+import java.util.AbstractSequentialList;
+import java.util.ListIterator;
+
+public class MyTwoWayLinkedList<E> extends AbstractSequentialList<E>{
+    private DLLNode<E> head, tail;
+    private int size =0;
+
+    /** Create a default list */
+    public MyTwoWayLinkedList() {
+    }
+
+    /** Create a list from an array of objects */
+    public MyTwoWayLinkedList(E[] objects) {
+        for (E e : objects)
+            add(e);
+    }
+
+    /** Return the head element in the list */
+    public E getFirst() {
+        if (size == 0) {
+            return null;
+        }
+        else {
+            return head.element;
+        }
+    }
+
+    /** Return the last element in the list */
+    public E getLast() {
+        if (size == 0) {
+            return null;
+        }
+        else {
+            return tail.element;
+        }
+    }
+
+    /** Add an element to the beginning of the list */
+    public void addFirst(E e) {
+        //add your code here
+        DLLNode<E> newNode = new DLLNode<>(e, null, null);
+    }
+
+    /** Add an element to the end of the list */
+    public void addLast(E e) {
+        
+		//add your code here
+
+    }
+
+    /**
+     * Add a new element at the specified index in this list The index of the
+     * head element is 0
+     */
+    public void add(int index, E e) {
+		
+        //Add your code here
+        
+    }
+
+    /**
+     * Remove the head node and return the object that is contained in the
+     * removed node.
+     */
+    public E removeFirst() {
+        
+		//Add your code here. 
+  
+        return null; // replace this with the correct return statement
+    }
+
+    /**
+     * Remove the last node and return the object that is contained in the
+     * removed node.
+     */
+    public E removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        else if (size == 1) {
+            DLLNode<E> temp = head;
+            head = tail = null;
+            size = 0;
+            return temp.element;
+        }
+        else {
+            DLLNode<E> temp = tail;
+            tail = tail.previous;
+            tail.next= null;
+            size--;
+            return temp.element;
+        }
+    }
+
+    /**
+     * Remove the element at the specified position in this list. Return the
+     * element that was removed from the list.
+     */
+    public E remove(int index) {
+        
+		//add code here. Remember to take care of the return null statement below
+ 
+        return null; // replace this with the correct return statement
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+
+        DLLNode<E> current = head;
+        for (int i = 0; i < size; i++) {
+            result.append(current.element);
+            current = current.next;
+            if (current != null) {
+                result.append(", "); // Separate two elements with a comma
+            }
+            else {
+                result.append("]"); // Insert the closing [] in the string
+            }
+        }
+
+        return result.toString();
+    }
+
+    /** Clear the list */
+    public void clear() {
+        head = tail = null;
+    }
+
+    /** Return true if this list contains the element o */
+    public boolean contains(Object e) {
+        for(DLLNode<E> current = head; current!=null; current = current.next){
+            if(current.element.equals(e))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Return the element from this list at the specified index */
+    public E get(int index) {
+        if (index < 0 || index > size-1 )
+            throw new IndexOutOfBoundsException
+                    ("Index: " + index + ", Size: " + size);
+        DLLNode current = head;
+        for(int i= 0; i< index;i++)
+            current = current.next;
+        return (E)current.element;
+    }
+
+    /**
+     * Return the index of the head matching element in this list. Return -1 if
+     * no match.
+     */
+    public int indexOf(Object e) {
+        int i=0;
+        for(DLLNode<E> current = head; current!=null; current = current.next){
+            if(e==current.element)
+                return i;
+            i++;
+        }
+        if (i < 0 || i > size-1 )
+            return -1;
+        else
+            return i;
+    }
+
+    /**
+     * Return the index of the last matching element in this list Return -1 if
+     * no match.
+     */
+    public int lastIndexOf(Object e) {
+        int i=0;
+        int n = -1;
+        for(DLLNode<E> current = head; current!=null; current = current.next){
+            if(e==current.element)
+                n=i;
+            i++;
+        }
+        return n;
+    }
+
+    /**
+     * Replace the element at the specified position in this list with the
+     * specified element.
+     */
+    public E set(int index, E e) {
+        if (index < 0 || index > size +1)
+        {
+            throw new IndexOutOfBoundsException
+                    ("Index: " + index + ", Size: " + size);
+        }
+        else{
+            DLLNode<E> current = head;
+            for(int i= 0; i< index -1;i++)
+                current = current.next;
+            current.next = new DLLNode<E>(e, current.next.next, current.next.next.previous);
+        }
+        return e;
+    }
+    /*Traverses the list from the tail to the head  */
+    public void traverseBackwards() {
+        ListIterator<E> itr = new LinkedListIterator<>(size -1);
+        while (itr.hasPrevious()) {
+            System.out.print(itr.previous() + " ");
+        }
+    }
+
+    /** Override the iterator method defined in Iterable */
+    public ListIterator<E> iterator() {
+        return new LinkedListIterator<E>();
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return new LinkedListIterator<E>(index);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private class DLLNode<E> {
+        E element;
+        DLLNode<E> next;
+        DLLNode<E> previous;
+        public DLLNode(){
+            next =null;
+        }
+        public  DLLNode(E el, DLLNode<E> nxt, DLLNode <E> prev){
+            element= el;
+            next = nxt;
+            previous = prev;
+        }
+    }
+
+    private class LinkedListIterator<E> implements ListIterator<E> {
+        private DLLNode<E> current = (DLLNode<E>)head; // Current index
+        int index = 0;
+        public LinkedListIterator() {
+
+        }
+
+        public LinkedListIterator(int index) {
+            if (index < 0 || index > size)
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size: "
+                        + size);
+            for (int nextIndex = 0; nextIndex < index; nextIndex++)
+                current = current.next;
+        }
+        @Override
+        public boolean hasNext() {
+            
+			//Add your code here;
+			
+			return false; // replace this with the correct return statement
+        }
+        @Override
+        public E next() {
+			
+            //add your code here
+            
+			return null; // replace this with the correct return statement
+        }
+        @Override
+        public boolean hasPrevious() {
+            
+			//add your code here
+			
+			return false;// replace this with the correct return statement
+        }
+        @Override
+        public E previous() {
+            
+			
+			//add your code here
+			
+            return null; // replace this with the correct return statement
+        }
+
+        public void remove() {
+            System.out.println("Implementation left as an exercise");
+        }
+
+        public void add(E e) {
+            System.out.println("Implementation left as an exercise");
+        }
+        public int nextIndex() {
+            System.out.println("Implementation left as an exercise");
+            return 0;
+        }
+        public int previousIndex() {
+            System.out.println("Implementation left as an exercise");
+            return 0;
+        }
+
+        @Override
+        public void set(E e) {
+            current.element = e;
+        }
+    }
+
+
+}
+
+
---
+++ Prac 4/Q1/Test_Q1.java
@@ -0,0 +1,44 @@
+public class Test_Q1 {
+
+    public static void main(String[] args) {
+    MyTwoWayLinkedList<Integer> list = new MyTwoWayLinkedList<Integer>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    list.add(7);
+    list.add(8);
+    list.add(9);
+    
+    System.out.println("Testing the ADD methods");
+    System.out.print("list :" + list);
+    list.addFirst(0);
+    System.out.print("\nlist :" + list);
+    list.addLast(Integer.SIZE);
+    System.out.print("\nlist :" + list);
+    list.add(list.size()/2, Integer.SIZE);
+    System.out.println("\nlist :" + list);    
+    System.out.println("list size is :"+ list.size());    
+    System.out.println("Element at 4 is :"+ list.get(4));
+    
+    System.out.println("\nTesting the REMOVE methods");
+    System.out.println("Element removed :"+ list.removeFirst());
+    System.out.println("list :" + list);
+    System.out.println("Element removed :"+ list.removeLast());
+    System.out.println("list :" + list); 
+    System.out.println("Element removed :"+ list.remove(5));
+    System.out.println("list :" + list);
+    System.out.print("\n");
+    
+    System.out.println("\nlist :" + list);
+    System.out.print("If all your methods are implemented correctly, this should return your" +
+    " current list in the reverse order with no elements skipped or added\n");
+    list.traverseBackwards();//If all your methods are implemented correctly, this should return your 
+                             // current list in the reverse order with no elements skipped or added.
+
+     
+    }
+}
+
