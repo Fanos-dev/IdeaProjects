d6f0636556620e4552e7c7908363a50d8ae51d4c
223030091 <akburallyirfan12@gmail.com>
2024-09-10 15:35:40 +0200

    Prac 4 Updated 5

--- Prac 4/Q2/Question2.java
+++ Prac 4/Q2/Question2.java
@@ -1,16 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.Stack;
+
 
 public class Question2 {
     public static void main(String[] args) {
         Stack<String> stack = new Stack<>();
+        
+        Scanner input = null;
+        try {
+            File file = new File("Test.java");
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
 
-        String str = "(Vishar{arli)n}0";
-
+        String str = "";
+        while (input.hasNextLine()) {
+            str += input.nextLine();
+        }
+        String current = "";
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(String.valueOf(ch));
+            if (!stack.isEmpty()) {
+                current = stack.peek();
+            }
+
+            if (ch == '(' || ch == '{' || ch == '[' || ch == ')' || ch == '}' || ch == ']') {
+                if (current.equals("(") && ch == ')') {
+                    stack.pop();
+                } else if (current.equals("{") && ch == '}') {
+                    stack.pop();
+                } else if (current.equals("[") && ch == ']') {
+                    stack.pop();
+                } else {
+                    stack.push(String.valueOf(ch));
+
+                }
             }
         }
 
