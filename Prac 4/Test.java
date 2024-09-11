90bf3b8d8961282e843e7837c589df7c5077b010
223030091 <akburallyirfan12@gmail.com>
2024-09-11 07:55:17 +0200

    Prac 4 Final 1

--- Prac 4/Test.java
+++ Prac 4/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World" + []);
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Hello World " + numbers[2]);
     }
 }
