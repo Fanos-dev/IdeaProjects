5a7d5533181a532ac8a0256b100b3113d6e53e9c
Irfan <50040808+Fanos-dev@users.noreply.github.com>
2024-09-16 18:44:12 +0200

    Prac 5 Start

---
+++ Prac 5/Q1/Test_Q1.java
@@ -0,0 +1,51 @@
+public class Test_Q1 {
+    public static void main(String[] args) {
+        // Create a BST
+        BST<String> tree = new BST<>();
+        tree.insert("Green");
+        tree.insert("Mabel");
+        tree.insert("Teal");
+        tree.insert("Blue");
+        tree.insert("Jade");
+        tree.insert("Purple"); // Insert Purple to the tree
+        tree.insert("Danube");
+
+        // Traverse tree
+        System.out.print("Inorder (sorted): ");
+        tree.inorder();
+        System.out.print("\nPostorder: ");
+        tree.postorder();
+        System.out.print("\nPreorder: ");
+        tree.preorder();
+        System.out.print("\nThe number of nodes is " + tree.getSize());
+
+        // Search for an element
+        System.out.print("\nIs Purple in the tree? " +
+                tree.search("Purple"));
+
+        // Get a path from the root to Purple
+        System.out.print("\nA path from the root to Purple is: ");
+        java.util.ArrayList<BST.TreeNode<String>> path
+                = tree.path("Purple");
+        for (int i = 0; path != null && i < path.size(); i++)
+            System.out.print(path.get(i).element + " ");
+
+        Integer[] numbers = {2, 4, 3, 1, 8, 5, 6, 7};
+        BST<Integer> intTree = new BST<>(numbers);
+        intTree.deleteByMerging(5);
+        System.out.println("Inorder (sorted) after deleting 5: ");
+        intTree.inorder();
+
+        System.out.println("\nThe height of the tree is: "+intTree.getHeight());
+
+        System.out.println("\nInorder after converting the tree to its mirror image: ");
+        intTree.convertToMirrorImage();
+        intTree.inorder();
+
+        System.out.println("\n\nRecursive post order: ");
+        intTree.postorder();
+
+        System.out.println("\nIterative post order: ");
+        intTree.iterativePostorder();
+    }
+}
